1a0715c5f814ce0fa8b2eb69973be5b0da071fcf
Jack <114423104+dig1234567@users.noreply.github.com>
2026-01-11 14:56:49 +0800

    Update home-component.js

--- client/src/components/home-component.js
+++ client/src/components/home-component.js
@@ -23,6 +23,7 @@ const HomeComponent = ({ currentUser, setCurrentUser }) => {
             2024 五月天 [回到那一天] 25 週年-北京演唱會｜演唱會 |
             國家體育場-鳥巢
           </p>
+          <p className="text-content">目前已售票完畢</p>
           <button onClick={handleButton} className="button">
             立即訂票
           </button>
@@ -34,7 +35,7 @@ const HomeComponent = ({ currentUser, setCurrentUser }) => {
           <p className="text-content2">
             周杰倫《嘉年華》2025 香港演唱會2025 | 啟德體育園主場館
           </p>
-          <p style={{ marginTop: "10px" }} className="text-content2">
+          <p style={{ marginTop: "10px" }} className="text-content">
             目前尚未開放購票
           </p>
           <button onClick={handleButton} className="button">
@@ -47,3 +48,4 @@ const HomeComponent = ({ currentUser, setCurrentUser }) => {
 };
 
 export default HomeComponent;
+
